164c49835d09d02af99d835533971574c33c6649
Alex Bardashevsky <mijiekc@icloud.com>
2018-11-09 10:28:27 +0200

    Initial Commit

---
+++ HomeWork - Swift - 29. UITableView Static Cells/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWork - Swift - 29. UITableView Static Cells
+//
+//  Created by Oleksandr Bardashevskyi on 11/9/18.
+//  Copyright © 2018 Oleksandr Bardashevskyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ HomeWork - Swift - 29. UITableView Static CellsTests/HomeWork___Swift___29__UITableView_Static_CellsTests.swift
@@ -0,0 +1,34 @@
+//
+//  HomeWork___Swift___29__UITableView_Static_CellsTests.swift
+//  HomeWork - Swift - 29. UITableView Static CellsTests
+//
+//  Created by Oleksandr Bardashevskyi on 11/9/18.
+//  Copyright © 2018 Oleksandr Bardashevskyi. All rights reserved.
+//
+
+import XCTest
+@testable import HomeWork___Swift___29__UITableView_Static_Cells
+
+class HomeWork___Swift___29__UITableView_Static_CellsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
